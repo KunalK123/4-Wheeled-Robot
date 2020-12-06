6725c108e44d22ccc206f9546697f1d73dcfe734
KunalK123 <70448227+KunalK123@users.noreply.github.com>
2020-12-06 14:10:28 -0500

    Update CtrlJoint.py

--- CtrlJoint.py
+++ CtrlJoint.py
@@ -1,1 +1,77 @@
+#!/usr/bin/env python3
+import rospy
+from gazebo_msgs.srv import ApplyJointEffort
+from gazebo_msgs.srv import GetJointProperties
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest
+import os
+import time as t
+
+msg_topic = '/gazebo/apply_joint_effort'
+joint_left_front = 'left_wheel_hinge_front'
+joint_right_front = 'right_wheel_hinge_front'
+joint_left_back = 'left_wheel_hinge_back'
+joint_right_back = 'right_wheel_hinge_back'
+
+msg_topic_feedback = 'gazebo/get_joint_properties'
+
+pub_feedback = rospy.ServiceProxy(msg_topic_feedback, GetJointProperties)
+
+rospy.init_node('dd_ctrl')
+pub = rospy.ServiceProxy(msg_topic,ApplyJointEffort)
+
+effort = 0.5
+start_time = rospy.Time(0,0)
+
+f = 0.25
+T = 1/f
+end_time = rospy.Time(T,0)
+rate = rospy.Rate(f)
+
+#my_service = rospy.Service('/service_example_topic', Trigger, trigger_response)
+
+robot_proxy = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+
+while True:
+      x = 0
+      z = raw_input("Input:")
+      global robot_proxy
+      a = GetModelStateRequest(model_name = 'robot')
+      a.model_name = "robot"
+      s = robot_proxy(a)
+      x = s.pose.position.x
+      y = s.pose.position.y
+      
+      if(z == 'w'):
+        effort = effort
+        pub(joint_left_front, effort, start_time, end_time)
+        pub(joint_right_front, effort, start_time, end_time)
+        pub(joint_left_back, effort, start_time, end_time)
+        pub(joint_right_back, effort, start_time, end_time)
+        #val = pub_feedback(joint_left)
+        #val2 = pub_feedback(joint_right)
+        #print("Left Wheel:", val.rate)
+        #print("Right Wheel:", val2.rate)
+        print(s)
+
+      if(z == 's'):
+        effort = -effort
+        pub(joint_left_front, effort, start_time, end_time)
+        pub(joint_right_front, effort, start_time, end_time)
+        pub(joint_left_back, effort, start_time, end_time)
+        pub(joint_right_back, effort, start_time, end_time)
+        #val = pub_feedback(joint_left)
+        #val2 = pub_feedback(joint_right)
+        #print("Left Wheel:", val.rate)
+        #print("Right Wheel:", val2.rate)
+        print(s)
+            
+      if(z == 'a'):
+        effort = effort
+        pub(joint_left_front, effort, start_time, end_time)
+        print(s)
+      
+      if(z == 'd'):
+        effort = effort
+        pub(joint_right_front, effort, start_time, end_time)
+        print(s)
 
